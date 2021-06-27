101a6682a9a1dd5b9440f420aa5322e27ea42dbf
김 소 현 <sooooooohyeon5@naver.com>
2021-06-27 23:24:58 +0900

    헤더

--- Wecan_projects/Wecan_project/settings.py
+++ Wecan_projects/Wecan_project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Wecan_projects.urls'
+ROOT_URLCONF = 'Wecan_project.urls'
 
 TEMPLATES = [
     {
